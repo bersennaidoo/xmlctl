1afc8486b47d412dff18303b6e8a0509781b7760
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-08-16 01:15:43 +0200

    add fmt

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+  "fmt"
   "github.com/bersennaidoo/xmlctl/cmd"
 )
 
 func main() {
 
 	cmd.Execute()
+	fmt.Println("Started")
 }
